87e243617fafa144ce82c32547a1670feb34632f
reneePURK <hii@huu.com>
2016-11-10 23:47:09 +0200

    redundant dependency removed

--- app/src/main/java/com/salamientertainment/youtubeshortcut/MainActivity.java
+++ app/src/main/java/com/salamientertainment/youtubeshortcut/MainActivity.java
@@ -1,11 +1,11 @@
 package com.salamientertainment.youtubeshortcut;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
